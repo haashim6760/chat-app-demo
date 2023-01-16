acb25c84a072746b054e13f61644144410b51d67
haashim6760 <haashim.6760@gmail.com>
2023-01-16 15:07:11 +0000

    Add new message chat

--- src/components/chat-room/Chat-room.tsx
+++ src/components/chat-room/Chat-room.tsx
@@ -1,22 +1,40 @@
+import { addDoc, collection } from "firebase/firestore";
 import React, { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { useFirebase } from "../../providers/FirebaseProvider";
+
+
+
 function Chatroom() {
 let [message, setMessage] = useState("");
+let {firestore} = useFirebase();
+let messageRef = collection(firestore!, `message-chat`);
+
+const sendNewMessage =async (e: {preventDefault: () => void}) => {
+    e.preventDefault()
+    addDoc(messageRef, {
+       messages : message
+    })
+
+}
 
     return (
         <article className="app-main-article">
 
             <div className="view-messages"></div>
+            <form className="new-message" onSubmit={sendNewMessage}>
             <div className="new-message">
                 <input type="text"
                 placeholder="Message"
                 value={message} 
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
                 setMessage(event.target.value)}
-             
-                
                 />
+            
+            <button className="send-button" type="submit">
+                Send
+            </button>
             </div>
+            </form>
         </article>
     )
         
