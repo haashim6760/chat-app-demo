8c82942adabf58a79fe7ed1b118203789ebbe19c
haashim6760 <haashim.6760@gmail.com>
2023-01-16 16:07:56 +0000

    Add firebase calls to chat-room

--- src/components/chat-room/Chat-room.tsx
+++ src/components/chat-room/Chat-room.tsx
@@ -1,39 +1,77 @@
-import { addDoc, collection } from "firebase/firestore";
-import React, { useState } from "react";
+import { addDoc, collection, DocumentData, onSnapshot, QueryDocumentSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { useFirebase } from "../../providers/FirebaseProvider";
-
+import { useUser } from "../../providers/UserProvider";
 
 
 function Chatroom() {
-let [message, setMessage] = useState("");
+
+let [newMessage, setNewMessage] = useState("");
+let [error, setError] = useState("");
+let [messages, setMessages] = useState<QueryDocumentSnapshot<DocumentData>[] | null>(null)
 let {firestore} = useFirebase();
-let messageRef = collection(firestore!, `message-chat`);
+let messageRef = collection(firestore!, `messages`);
+let {user} = useUser();
+useEffect(() => {
+    if (user && firestore){
+        let allMessagesCollection = collection(firestore!, `messages`);
+        
+        onSnapshot(allMessagesCollection, (snapshot) => {
+            setMessages (snapshot.docs.sort((a,b) => b.data().message_chat - a.data().message_chat));
+        })
+    }});
+
 
 const sendNewMessage =async (e: {preventDefault: () => void}) => {
     e.preventDefault()
+    try{
     addDoc(messageRef, {
-       messages : message
+       message_chat : newMessage
     })
-
+    } catch {
+        setError("There Was An Error Sending Your Message");
+    }
 }
 
     return (
         <article className="app-main-article">
-
-            <div className="view-messages"></div>
+            {/* <div className="view-messages"> */}
             <form className="new-message" onSubmit={sendNewMessage}>
+                <table>
+            {/* {messages?.map((entry) =>     
+                        <td>{entry.data().message_chat}</td>
+                )} */}
+
+{messages?.map((entry) => {
+                  try {
+                    return entry.data().message_chat ? (
+                      <>
+                        <tr key={entry.id}>
+                          <td>{entry.data().message_chat}</td>
+                        </tr>
+                        {console.log("NOW")}
+                      </>
+                    ) : (
+                      <tr></tr>
+                    );
+                  } catch (e: any) {
+                    return;
+                  }
+                })}
+            {/* </div> */}
+            
             <div className="new-message">
                 <input type="text"
                 placeholder="Message"
-                value={message} 
+                value={newMessage} 
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                setMessage(event.target.value)}
+                setNewMessage(event.target.value)}
                 />
             
             <button className="send-button" type="submit">
                 Send
             </button>
-            </div>
+            </div></table>
             </form>
         </article>
     )
